17919a814b83ecad9e9fda0d355f2eaca44d5408
jfbs <phrewn@gmail.com>
2017-10-21 15:21:14 -0400

    init

---
+++ history.py
@@ -0,0 +1,75 @@
+# Downloading daily price-series OHCL for Cryptocurrencies
+# (c) 2017 QuantAtRisk.com, by Pawel Lachowicz
+ 
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from datetime import datetime
+import json
+from bs4 import BeautifulSoup
+import requests
+ 
+def timestamp2date(timestamp):
+    # function converts a Unix timestamp into Gregorian date
+    return datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d')
+ 
+def date2timestamp(date):
+    # function coverts Gregorian date in a given format to timestamp
+    return datetime.strptime(date_today, '%Y-%m-%d').timestamp()
+ 
+ 
+def fetchCryptoOHLC(fsym, tsym):
+    # function fetches a crypto price-series for fsym/tsym and stores
+    # it in pandas DataFrame
+ 
+    cols = ['date', 'timestamp', 'open', 'high', 'low', 'close']
+    lst = ['time', 'open', 'high', 'low', 'close']
+ 
+    timestamp_today = datetime.today().timestamp()
+    curr_timestamp = timestamp_today
+ 
+    for j in range(2):
+        df = pd.DataFrame(columns=cols)
+        url = "https://min-api.cryptocompare.com/data/histoday?fsym=" + fsym + \
+         "&tsym=" + tsym + "&toTs=" + str(int(curr_timestamp)) + "&limit=2000"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        dic = json.loads(soup.prettify())
+        for i in range(1, 2001):
+            tmp = []
+            for e in enumerate(lst):
+                x = e[0]
+                y = dic['Data'][i][e[1]]
+                if(x == 0):
+                    tmp.append(str(timestamp2date(y)))
+                tmp.append(y)
+            if(np.sum(tmp[-4::]) > 0):
+                df.loc[len(df)] = np.array(tmp)
+        df.index = pd.to_datetime(df.date)
+        df.drop('date', axis=1, inplace=True)
+        curr_timestamp = int(df.ix[0][0])
+        if(j == 0):
+            df0 = df.copy()
+        else:
+            data = pd.concat([df, df0], axis=0)
+ 
+    return data
+
+# implement
+fsym = "BTC"
+tsym = "USD"
+data = fetchCryptoOHLC(fsym, tsym)
+ 
+# print the BTC/USD OHLC price-series
+print(data)
+ 
+# plot them all
+plt.figure(figsize=(10,4))
+plt.plot(data.open)
+plt.plot(data.high)
+plt.plot(data.low)
+plt.plot(data.close)
+plt.legend(loc=2)
+plt.title(fsym, fontsize=12)
+plt.ylabel(tsym, fontsize=12)
+plt.show()
---
+++ marketCap.py
@@ -0,0 +1,31 @@
+# Getting a List of Cryptocurrencies sorted by their most current Market Cap
+# (c) 2017 QuantAtRisk.com, by Pawel Lachowicz
+# src: http://www.quantatrisk.com/2017/03/20/download-crypto-currency-time-series-portfolio-python/
+ 
+import pandas as pd
+import json
+from bs4 import BeautifulSoup
+import requests
+ 
+url = "https://api.coinmarketcap.com/v1/ticker/"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+dic = json.loads(soup.prettify())
+ 
+# create an empty DataFrame
+df = pd.DataFrame(columns=["Ticker", "MarketCap"])
+ 
+for i in range(len(dic)):
+    df.loc[len(df)] = [dic[i]['symbol'], dic[i]['market_cap_usd']]
+ 
+df.sort_values(by=['MarketCap'])
+# apply conversion to numeric as 'df' contains lots of 'None' string as values
+df.MarketCap = pd.to_numeric(df.MarketCap)
+
+# print(df.MarketCap)
+
+P = df[df.MarketCap > 20e6]
+print(P,)
+ 
+portfolio = list(P.Ticker)
+print(portfolio)
